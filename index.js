4a5529716819a25b0489be6640660034c004aeb9
Tziporah <tziporawerzberger@gmail.com>
2021-03-03 14:41:01 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <ul> 
+    <li>Hello</li>
+    <li>My name is</li>
+    <li>Tzipora</li>
+  </ul>, 
+  document.getElementById("root"));
